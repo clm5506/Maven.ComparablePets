970333306a26b6c76019026f10f400163aa58541
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-20 21:03:55 -0500

    Tests written hopefully XD

--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,5 +1,186 @@
 package io.zipcoder;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ApplicationTest {
+
+    // Getter Tests
+    @Test
+    public void getDogTest
+
+    {
+        String name = "Pogo"
+        Dog dog = new Dog(name);
+
+        String actual = dog.getName();
+        Assert.assertEquals(name, actual);
+    }
+
+    @Test
+    public void getCatTest
+
+    {
+        String name = "Momo"
+        Cat cat = new Cat(name);
+
+        String actual = cat.getName();
+        Assert.assertEquals(name, actual);
+    }
+
+    @Test
+    public void getHamsterTest
+
+    {
+        String name = "Hamtaro"
+        Hamster hamster = new Hamster(name);
+
+        String actual = hamster.getName();
+        Assert.assertEquals(name, actual);
+    }
+
+
+    // Setter Tests
+
+    @Test
+    public void setDogTest
+
+    {
+        String name = "Pogo"
+        Dog dog = new Dog(name);
+        String newName = "Blue";
+        dog.setName(newName);
+
+
+        String actual = dog.getName();
+        Assert.assertEquals(newName, actual);
+    }
+
+    @Test
+    public void getCatTest
+
+    {
+        String name = "Momo"
+        Cat cat = new Cat(name);
+        String newName = "Momomomo";
+        cat.setName(newName);
+
+        String actual = cat.getName();
+        Assert.assertEquals(newName, actual);
+    }
+
+    @Test
+    public void getHamsterTest
+
+    {
+        String name = "Hamtaro"
+        Hamster hamster = new Hamster(name);
+        String newName = "Tamagotchi";
+        hamster.setName(newName);
+
+        String actual = hamster.getName();
+        Assert.assertEquals(newName, actual);
+    }
+
+    // Speak Tests
+
+    @Test
+    public void speakDogTest
+
+    {
+        String name = "Pogo"
+        Dog dog = new Dog(name);
+
+        String expected = "Bark!";
+        String actual = dog.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCatTest
+
+    {
+        String name = "Momo"
+        Cat cat = new Cat(name);
+
+        String expected = "Meow!";
+        String actual = cat.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHamsterTest
+
+    {
+        String name = "Hamtaro"
+        Hamster hamster = new Hamster(name);
+
+        String expected = "meep";
+        String actual = hamster.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
+    // Sort Method Tests
+
+    @Test
+    public void sortPetTest{
+        Pet pet1 = new Cat("Momo");
+        Pet pet2 = new Dog("Old Yeller");
+        Pet pet3 = new Hamster("Meepers Jeepers");
+
+        Pet[] input = {pet2, pet3, pet1};
+        Pet[] expected = {pet1, pet2, pet3};
+
+        PetSorter sorter = new PerSorter(input);
+        Pet[] actual = sorter.sort();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    // compareTo Tests
+
+    @Test
+    public void compareToNegativeTest{
+        Pet pet1 = new Cat("Momo");
+        Pet pet2 = new Dog("Old Yeller");
+
+        boolean actual = (pet1.compareTo(pet2) < 0);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void compareToNegativeTest2{
+        Pet pet1 = new Cat("Momo");
+        Pet pet2 = new Dog("Momo");
+
+        boolean actual = (pet1.compareTo(pet2) < 0);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void compareToNeutralTest{
+        Pet pet1 = new Cat("Momo");
+        Pet pet2 = new Cat("Momo");
+
+        boolean actual = (pet1.compareTo(pet2) == 0);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void compareToPositiveTest1{
+        Pet pet1 = new Cat("Thomas");
+        Pet pet2 = new Dog("Momo");
+
+        boolean actual = (pet1.compareTo(pet2) > 0);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void compareToPositiveTest2{
+        Pet pet1 = new Hamster("Momo");
+        Pet pet2 = new Dog("Momo");
+
+        boolean actual = (pet1.compareTo(pet2) > 0);
+        Assert.assertTrue(actual);
+    }
 }
